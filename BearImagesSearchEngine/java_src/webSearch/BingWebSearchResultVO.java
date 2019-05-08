f9eb8b6d18ff5e79a6a8630dee00af092135bd33
apineda4 <apineda4@alaska.edu>
2019-05-07 16:52:04 -0800

    Finish this

---
+++ BearImagesSearchEngine/java_src/webSearch/BingWebSearchResultVO.java
@@ -0,0 +1,18 @@
+package webSearch;
+
+import java.util.Date;
+
+public class BingWebSearchResultVO
+{
+	private String webSearchUrl;
+	private String name;
+	private String thumbnailUrl;
+	private Date datePublished;
+	private String contentUrl;
+	private String hostPageUrl;
+	private String contentSize;
+	private String encodingFormat;
+	private String hostPageDisplayUrl;
+	private int width;
+	private int height;
+}
