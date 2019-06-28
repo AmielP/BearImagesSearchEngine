3e18c5db70a62b0032922478156ca09a2f3bb803
apineda4 <apineda4@alaska.edu>
2019-06-28 15:10:27 -0800

    Tighten security on websites and filter more illegal images better

--- BearImagesSearchEngine/java_src/webSearch/dao/BingWebSearchResult.java
+++ BearImagesSearchEngine/java_src/webSearch/dao/BingWebSearchResult.java
@@ -118,40 +118,51 @@ public class BingWebSearchResult
 
 		for(int i = 0; i < jsonArray.size(); i++)
 		{
-			BingWebSearchResultVO bingWebSearchData = new BingWebSearchResultVO();
+			try
+			{
+				BingWebSearchResultVO bingWebSearchData = new BingWebSearchResultVO();
 
-			jsonObjectResult = jsonArray.get(i).getAsJsonObject();
+				jsonObjectResult = jsonArray.get(i).getAsJsonObject();
 
-			// Print each of the URL results
-			System.out.println("Result " + (i + 1) + ": " + jsonObjectResult.get("contentUrl").getAsString());
-//			bingWebSearchData.setContentID((i + 1));
-			bingWebSearchData.setContentUrl(jsonObjectResult.get("contentUrl").getAsString());
-			bingWebSearchData.setName(jsonObjectResult.get("name").getAsString());
+				// Print each of the URL results
+				System.out.println("Result " + (i + 1) + ": " + jsonObjectResult.get("contentUrl").getAsString());
+				URL imageData = new URL(jsonObjectResult.get("contentUrl").getAsString());
+				ImageIO.read(imageData);
+				//			bingWebSearchData.setContentID((i + 1));
+				bingWebSearchData.setContentUrl(jsonObjectResult.get("contentUrl").getAsString());
+				bingWebSearchData.setName(jsonObjectResult.get("name").getAsString());
 
-			//			String s
-			//			
-			//			bingWebSearchData.setImageData(jsonObjectResult.get("contentUrl").getAsString());
+				//			String s
+				//			
+				//			bingWebSearchData.setImageData(jsonObjectResult.get("contentUrl").getAsString());
 
-			//			try
-			//			{
-			//				BufferedImage bufferedImage = ImageIO.read(new URL(jsonObjectResult.get("contentUrl").getAsString()));
-			//				
-			//				ImageIO.write(bufferedImage, "jpg", new File("D://BearImages/" + i + ".jpg"));
-			//			} 
-			//			catch (MalformedURLException e)
-			//			{
-			//				// TODO Auto-generated catch block
-			//				e.printStackTrace();
-			//				throw e;
-			//			} 
-			//			catch (IOException e)
-			//			{
-			//				// TODO Auto-generated catch block
-			//				e.printStackTrace();
-			//				throw e;
-			//			}
+				//			try
+				//			{
+				//				BufferedImage bufferedImage = ImageIO.read(new URL(jsonObjectResult.get("contentUrl").getAsString()));
+				//				
+				//				ImageIO.write(bufferedImage, "jpg", new File("D://BearImages/" + i + ".jpg"));
+				//			} 
+				//			catch (MalformedURLException e)
+				//			{
+				//				// TODO Auto-generated catch block
+				//				e.printStackTrace();
+				//				throw e;
+				//			} 
+				//			catch (IOException e)
+				//			{
+				//				// TODO Auto-generated catch block
+				//				e.printStackTrace();
+				//				throw e;
+				//			}
 
-			headerList.add(bingWebSearchData);
+				headerList.add(bingWebSearchData);
+			}
+			catch (Exception e)
+			{
+				System.out.println("test");
+				i++;
+				continue;
+			}
 		}
 		return headerList;
 	}
@@ -221,40 +232,45 @@ public class BingWebSearchResult
 	public void createWebResultsTable() throws SQLException
 	{
 		Connection connection = null;
-		Statement preparedStatement = null;
-		
-		String sql = "CREATE TABLE IF NOT EXISTS WEB_SEARCH_RESULT ( "
+		Statement createTempTablePreparedStatement = null;
+		Statement createWebSearchResultTablePreparedStatement = null;
+
+		String createTempTableSql = "CREATE TABLE IF NOT EXISTS TEMP_TABLE (CONTENT_URL TEXT , NAME TEXT, CONSTRAINT CONTENT_URL_UNIQUE UNIQUE (CONTENT_URL) ON CONFLICT REPLACE)";
+		String createWebSearchResultSql = "CREATE TABLE IF NOT EXISTS WEB_SEARCH_RESULT ( "
 				+ " WEB_SEARCH_RESULT_ID INTEGER PRIMARY KEY, "
 				+ " CONTENT_URL TEXT, "
 				+ " NAME TEXT, "
 				+ " CONSTRAINT CONTENT_URL_UNIQUE UNIQUE (CONTENT_URL) ON CONFLICT REPLACE"
 				+ ");";
 
-//		String sql = "CREATE TABLE IF NOT EXISTS WEB_SEARCH_RESULT ( "
-//				+ " WEB_SEARCH_RESULT_ID INTEGER PRIMARY KEY, "
-//				+ " CONTENT_URL TEXT, "
-//				+ " NAME TEXT, "
-//				+ " IMAGE_DATA BLOB"
-//				+ ")";
+		//		String sql = "CREATE TABLE IF NOT EXISTS WEB_SEARCH_RESULT ( "
+		//				+ " WEB_SEARCH_RESULT_ID INTEGER PRIMARY KEY, "
+		//				+ " CONTENT_URL TEXT, "
+		//				+ " NAME TEXT, "
+		//				+ " IMAGE_DATA BLOB"
+		//				+ ")";
 		try
 		{
 			connection = ConnectionHelper.getConnection();
-			preparedStatement = connection.createStatement();
+			createTempTablePreparedStatement = connection.createStatement();
+			createWebSearchResultTablePreparedStatement = connection.createStatement();
 			//			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.execute(sql);
+			createTempTablePreparedStatement.execute(createTempTableSql);
+			createWebSearchResultTablePreparedStatement.execute(createWebSearchResultSql);
 		}
 		catch (SQLException e)
 		{
-			System.out.println("ERROR: createWebResultsTable");
+			System.out.println("ERROR: createWebResultsTable or createTempResultsTable");
 			System.out.println(e.getMessage() + "\n");
 		}
 		finally
 		{
-			if (preparedStatement != null)
+			if (createWebSearchResultTablePreparedStatement != null || createTempTablePreparedStatement != null)
 			{
 				try
 				{
-					preparedStatement.close();
+					createTempTablePreparedStatement.close();
+					createWebSearchResultTablePreparedStatement.close();
 				}
 				catch (SQLException ignore) {}
 			}
@@ -274,10 +290,10 @@ public class BingWebSearchResult
 		String sql = "SELECT WEB_SEARCH_RESULT_ID, CONTENT_URL, NAME\r\n" + 
 				"FROM WEB_SEARCH_RESULT\r\n" + 
 				"WHERE CONTENT_URL NOT IN (SELECT ILLEGAL_CONTENT_URL FROM ILLEGAL_WEBSITE)";
-//		String sql = "SELECT WEB_SEARCH_RESULT_ID, CONTENT_URL, NAME, IMAGE_DATA FROM WEB_SEARCH_RESULT";
-//		String sql = "SELECT WEB_SEARCH_RESULT_ID, IMAGE_DATA, NAME FROM WEB_SEARCH_RESULT";
-//		String sql = "SELECT WEB_SEARCH_RESULT_ID, CONTENT_URL, NAME FROM WEB_SEARCH_RESULT";
-		
+		//		String sql = "SELECT WEB_SEARCH_RESULT_ID, CONTENT_URL, NAME, IMAGE_DATA FROM WEB_SEARCH_RESULT";
+		//		String sql = "SELECT WEB_SEARCH_RESULT_ID, IMAGE_DATA, NAME FROM WEB_SEARCH_RESULT";
+		//		String sql = "SELECT WEB_SEARCH_RESULT_ID, CONTENT_URL, NAME FROM WEB_SEARCH_RESULT";
+
 		try
 		{
 			connection = ConnectionHelper.getConnection();
@@ -286,20 +302,20 @@ public class BingWebSearchResult
 			while(resultSet.next())
 			{
 				BingWebSearchResultVO data = new BingWebSearchResultVO();
-				
+
 				// Can be in any order of setting I want so long as the indexing starts from index 1,
 				// the data types match the variable type you are setting,
 				// and the index positions in the VO match the datagrid
 				data.setContentID(resultSet.getInt(1));
 				data.setContentUrl(resultSet.getString(2));
 				data.setName(resultSet.getString(3));
-				
+
 				System.out.println("Selecting from " + data.getContentUrl());
-//				data.setImageData(resultSet.getBytes(4));
-				
-//				data.setContentID(resultSet.getInt(1));
-//				data.setImageData(resultSet.getBytes(2));
-//				data.setName(resultSet.getString(3));
+				//				data.setImageData(resultSet.getBytes(4));
+
+				//				data.setContentID(resultSet.getInt(1));
+				//				data.setImageData(resultSet.getBytes(2));
+				//				data.setName(resultSet.getString(3));
 
 				list.add(data);
 			}
@@ -327,27 +343,40 @@ public class BingWebSearchResult
 	public void insertWebResultsData(ArrayList<BingWebSearchResultVO> insertList) throws SQLException
 	{
 		Connection connection = null;
+		PreparedStatement tempPreparedStatement = null;
 		PreparedStatement legalPreparedStatement = null;
 		PreparedStatement illegalPreparedStatement = null;
-		
+
+		ResultSet resultSet = null;
+		PreparedStatement selectTempPreparedStatement = null;
+
 		int total = 0;
-		
-		
-		String legalSql = "INSERT INTO WEB_SEARCH_RESULT(CONTENT_URL, NAME) "
-				+ "VALUES(?,?) "
-				+ "UNION SELECT CONTENT_URL, NAME FROM WEB_SEARCH_RESULT "
-				+ "WHERE CONTENT_URL NOT IN (SELECT ILLEGAL_CONTENT_URL FROM ILLEGAL_WEBSITE)";
-		
+
+
+		//		String legalSql = "INSERT INTO WEB_SEARCH_RESULT(CONTENT_URL, NAME) "
+		//				+ "VALUES(?,?) "
+		//				+ "UNION SELECT CONTENT_URL, NAME FROM WEB_SEARCH_RESULT "
+		//				+ "WHERE CONTENT_URL NOT IN (SELECT ILLEGAL_CONTENT_URL FROM ILLEGAL_WEBSITE)";
+
+		String tempSql = "INSERT INTO TEMP_TABLE (CONTENT_URL, NAME) VALUES (?,?)";
+
+		String legalSql = "INSERT INTO WEB_SEARCH_RESULT (CONTENT_URL, NAME) SELECT CONTENT_URL, NAME FROM TEMP_TABLE as t WHERE NOT EXISTS (SELECT * FROM ILLEGAL_WEBSITE as iw WHERE t.CONTENT_URL = iw.ILLEGAL_CONTENT_URL)";
+		//		String legalSql = "INSERT INTO WEB_SEARCH_RESULT (CONTENT_URL, NAME) VALUES (?,?) EXCEPT SELECT ILLEGAL_CONTENT_URL, NAME FROM ILLEGAL_WEBSITE";
+
+		//		String selectTempSql = "SELECT CONTENT_URL, NAME FROM TEMP_TABLE";
+
 		String illegalSql = "INSERT INTO ILLEGAL_WEBSITE (ILLEGAL_CONTENT_URL, NAME) "
 				+ "VALUES (?,?)";
-		
-//		String sql = "INSERT INTO WEB_SEARCH_RESULT (CONTENT_URL, NAME, IMAGE_DATA) "
-//				+ "VALUES (?,?,?)";
+		//		int legalCount = 0;
+		//		String sql = "INSERT INTO WEB_SEARCH_RESULT (CONTENT_URL, NAME, IMAGE_DATA) "
+		//				+ "VALUES (?,?,?)";
 		try
 		{
 			connection = ConnectionHelper.getConnection();
+			tempPreparedStatement = connection.prepareStatement(tempSql);
 			legalPreparedStatement = connection.prepareStatement(legalSql);
 			illegalPreparedStatement = connection.prepareStatement(illegalSql);
+
 
 			for(int i = 0; i < insertList.size() /*NUMBER_OF_MAX_SEARCHES*/; i++)
 			{
@@ -360,43 +389,44 @@ public class BingWebSearchResult
 					System.out.println(i + ". new URL(): " + insertList.get(i).getContentUrl());
 					// BLOB for later.
 					URL imageData = new URL(insertList.get(i).getContentUrl());
-					
-//					BufferedImage bufferedImage = ImageIO.read(imageData);
-//					ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//					ImageIO.write(bufferedImage, "jpg", byteArrayOutputStream);
+
+					//					BufferedImage bufferedImage = ImageIO.read(imageData);
+					//					ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+					//					ImageIO.write(bufferedImage, "jpg", byteArrayOutputStream);
 					ImageIO.read(imageData);
-//					byte[] imageDataToByteArray = byteArrayOutputStream.toByteArray();
-					
-//					preparedStatement.setInt(1, insertList.get(i).getContentID());
-					legalPreparedStatement.setString(1, insertList.get(i).getContentUrl());
-					legalPreparedStatement.setString(2, insertList.get(i).getName());
-//					preparedStatement.setBytes(3, imageDataToByteArray);
-//					preparedStatement.setBytes(4, insertList.get(i).getImageData());
-					legalPreparedStatement.addBatch();
-				
+					//					byte[] imageDataToByteArray = byteArrayOutputStream.toByteArray();
+
+					//					preparedStatement.setInt(1, insertList.get(i).getContentID());
+					tempPreparedStatement.setString(1, insertList.get(i).getContentUrl());
+					tempPreparedStatement.setString(2, insertList.get(i).getName());
+					//					preparedStatement.setBytes(3, imageDataToByteArray);
+					//					preparedStatement.setBytes(4, insertList.get(i).getImageData());
+					tempPreparedStatement.addBatch();
+
 					total++;
+					//					legalCount++;
 					System.out.println("TOTAL: " + total);
 				}
-//				catch (IOException ioe)
-//				{
-//					// Add link to ILLEGAL_LINK table
-//					// Skip to next link
-////					System.out.println("OOPSIE: image reading problem");
-////					System.out.println(ioe.getMessage());
-////					ioe.printStackTrace(System.out);
-//					i++;
-//					continue;
-//				}
-//				catch (NullPointerException npe)
-//				{
-//					// Add link to ILLEGAL_LINK table
-//					// Skip to next link
-////					System.out.println("OOPSIE: image reading problem");
-////					System.out.println(ioe.getMessage());
-////					ioe.printStackTrace(System.out);
-//					i++;
-//					continue;
-//				}
+				//				catch (IOException ioe)
+				//				{
+				//					// Add link to ILLEGAL_LINK table
+				//					// Skip to next link
+				////					System.out.println("OOPSIE: image reading problem");
+				////					System.out.println(ioe.getMessage());
+				////					ioe.printStackTrace(System.out);
+				//					i++;
+				//					continue;
+				//				}
+				//				catch (NullPointerException npe)
+				//				{
+				//					// Add link to ILLEGAL_LINK table
+				//					// Skip to next link
+				////					System.out.println("OOPSIE: image reading problem");
+				////					System.out.println(ioe.getMessage());
+				////					ioe.printStackTrace(System.out);
+				//					i++;
+				//					continue;
+				//				}
 				catch (Exception e)
 				{
 					illegalPreparedStatement.setString(1, insertList.get(i).getContentUrl());
@@ -407,14 +437,22 @@ public class BingWebSearchResult
 					continue;
 				}
 			}
-			legalPreparedStatement.executeBatch();
+			tempPreparedStatement.executeBatch();
 			illegalPreparedStatement.executeBatch();
+
+			legalPreparedStatement.executeUpdate();
 		}
-//		catch (IOException ioe)
-//		{
-//			System.out.println("ERROR: image reading problem");
-//			System.out.println(ioe.getMessage());
-//		}
+		catch (ArrayIndexOutOfBoundsException aoe)
+		{
+			System.out.print("ERROR: Array out of bounds exception");
+			aoe.printStackTrace(System.out);
+			System.out.println(aoe.getMessage());
+		}
+		//		catch (IOException ioe)
+		//		{
+		//			System.out.println("ERROR: image reading problem");
+		//			System.out.println(ioe.getMessage());
+		//		}
 		catch (SQLException sqle)
 		{
 			System.out.println("ERROR: insertWebResultsData");
@@ -422,10 +460,11 @@ public class BingWebSearchResult
 		}
 		finally
 		{
-			if (legalPreparedStatement != null || illegalPreparedStatement != null)
+			if (tempPreparedStatement != null || illegalPreparedStatement != null || legalPreparedStatement != null)
 			{
 				try
 				{
+					tempPreparedStatement.close();
 					legalPreparedStatement.close();
 					illegalPreparedStatement.close();
 				}
@@ -490,7 +529,7 @@ public class BingWebSearchResult
 		try
 		{
 			connection = ConnectionHelper.getConnection();
-			
+
 			if (isNewDataInTable)
 			{
 				preparedStatement = connection.prepareStatement(insertSQL);
@@ -499,7 +538,7 @@ public class BingWebSearchResult
 			{
 				preparedStatement = connection.prepareStatement(updateSQL);
 			}
-			
+
 			for (int i = 0; i < NUMBER_OF_MAX_SEARCHES; i++)
 			{
 				preparedStatement.setString(1, insertAndUpdateList.get(i).getContentUrl());
@@ -574,7 +613,7 @@ public class BingWebSearchResult
 			//			ConnectionHelper.close(connection);
 		}
 	}
-	
+
 	public void deleteWebResultsData() throws SQLException
 	{
 		Connection connection = null;
@@ -584,7 +623,7 @@ public class BingWebSearchResult
 		{
 			connection = ConnectionHelper.getConnection();
 			preparedStatement = connection.prepareStatement(sql);
-			
+
 			preparedStatement.executeUpdate();
 		}
 		catch (SQLException e)
@@ -604,18 +643,23 @@ public class BingWebSearchResult
 			}
 		}
 	}
-	
+
 	public void dropTable() throws SQLException
 	{
 		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		String sql = "DROP TABLE IF EXISTS WEB_SEARCH_RESULT";
+		PreparedStatement dropWebSearchResultPreparedStatement = null;
+		PreparedStatement dropTempTablePreparedStatement = null;
+		String dropWebSearchResultSql = "DROP TABLE IF EXISTS WEB_SEARCH_RESULT";
+		String dropTempTableSql = "DROP TABLE IF EXISTS TEMP_TABLE";
 		try
 		{
 			connection = ConnectionHelper.getConnection();
-			preparedStatement = connection.prepareStatement(sql);
-			
-			preparedStatement.executeUpdate();
+			dropWebSearchResultPreparedStatement = connection.prepareStatement(dropWebSearchResultSql);
+			dropTempTablePreparedStatement = connection.prepareStatement(dropTempTableSql);
+
+			dropWebSearchResultPreparedStatement.executeUpdate();
+
+			dropTempTablePreparedStatement.executeUpdate();
 		}
 		catch (SQLException e)
 		{
@@ -624,11 +668,12 @@ public class BingWebSearchResult
 		}
 		finally
 		{
-			if (preparedStatement != null)
+			if (dropWebSearchResultPreparedStatement != null || dropTempTablePreparedStatement != null)
 			{
 				try
 				{
-					preparedStatement.close();
+					dropWebSearchResultPreparedStatement.close();
+					dropTempTablePreparedStatement.close();
 				}
 				catch(SQLException ignore) {}
 			}
